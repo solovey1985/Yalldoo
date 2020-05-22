63261a5cbe2154561d9cf532648ebb3a54380a85
oleg-gonchar <oleg.gonchar@valtech.com>
2020-05-22 17:27:14 +0300

    Navbar transparency

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -14,6 +14,19 @@ export class LandingComponent implements OnInit {
   ngOnInit() {
     var rellaxHeader = new Rellax('.rellax-header');
     var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.add('navbar-transparent');
+    window.addEventListener('scroll', function() {
+      if (window.pageYOffset > 200 ) {
+        navbar.classList.remove('navbar-transparent');
+      } else {
+        navbar.classList.add('navbar-transparent');
+      }
+
+      if (body.scrollHeight - window.pageYOffset < 1100 ) {
+        navbar.classList.remove('nav-up');
+        navbar.classList.add('nav-down');
+      }
+    });
     navbar.classList.add('navbar-landing');
     var body = document.getElementsByTagName('body')[0];
     body.classList.add('landing-page');

0ca1398ff42ddf356e023ff57f0a988b94bbe15e
oleg-gonchar <oleg.gonchar@valtech.com>
2020-05-22 16:04:55 +0300

    Landing navbar changing. White backgrount to lightgrey. Little button changes

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -14,13 +14,13 @@ export class LandingComponent implements OnInit {
   ngOnInit() {
     var rellaxHeader = new Rellax('.rellax-header');
     var navbar = document.getElementsByTagName('nav')[0];
-    navbar.classList.add('navbar-transparent');
+    navbar.classList.add('navbar-landing');
     var body = document.getElementsByTagName('body')[0];
     body.classList.add('landing-page');
   }
   ngOnDestroy(){
     var navbar = document.getElementsByTagName('nav')[0];
-    navbar.classList.remove('navbar-transparent');
+    navbar.classList.remove('navbar-landing');
     var body = document.getElementsByTagName('body')[0];
     body.classList.remove('landing-page');
   }

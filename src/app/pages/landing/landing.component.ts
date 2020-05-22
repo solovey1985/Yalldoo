d6986a7ea49fde4f6e42d71fe1d2b72efa622b9b
oleg-gonchar <oleg.gonchar@valtech.com>
2020-05-22 19:07:32 +0300

    Smoothly scrolling from nav to blocks

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -13,8 +13,38 @@ export class LandingComponent implements OnInit {
 
   ngOnInit() {
     var rellaxHeader = new Rellax('.rellax-header');
+
     var navbar = document.getElementsByTagName('nav')[0];
-    navbar.classList.add('navbar-transparent');
+    var body = document.getElementsByTagName('body')[0];
+
+    var descriptionElement = document.querySelector('.js-yld-about');
+    var teamElement = document.querySelector('.js-yld-team');
+    var contactUsElement = document.querySelector('.js-yld-contactus');
+    var loginElement = document.querySelector('.js-yld-login');
+
+    function scrollTo(element, to, duration) {
+      if (duration <= 0) return;
+      var difference = to - element.scrollTop;
+      var perTick = difference / duration * 5;
+      setTimeout(function() {
+          element.scrollTop = element.scrollTop + perTick;
+          if (element.scrollTop === to) return;
+          scrollTo(element, to, duration - 5);
+      }, 10);
+    }
+
+    [descriptionElement, teamElement, contactUsElement, loginElement].map(function(element){
+      element.addEventListener('click', function() {
+        var anchorClass = '.js-yld-anchor-' + this.attributes['data-anchor'].nodeValue;
+        var elementToScroll = document.querySelector(anchorClass);
+        var scrollPoint = elementToScroll.getBoundingClientRect().top + window.scrollY;
+        scrollTo(document.documentElement, scrollPoint, 200);
+      });
+    });
+
+    navbar.classList.add('navbar-transparent', 'navbar-landing');
+    body.classList.add('landing-page');
+
     window.addEventListener('scroll', function() {
       if (window.pageYOffset > 200 ) {
         navbar.classList.remove('navbar-transparent');
@@ -27,9 +57,8 @@ export class LandingComponent implements OnInit {
         navbar.classList.add('nav-down');
       }
     });
-    navbar.classList.add('navbar-landing');
-    var body = document.getElementsByTagName('body')[0];
-    body.classList.add('landing-page');
+
+    // element.scrollIntoView(alignToTop)
   }
   ngOnDestroy(){
     var navbar = document.getElementsByTagName('nav')[0];

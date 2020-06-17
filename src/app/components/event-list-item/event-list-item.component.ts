c9a07cfc3bcd9b41948dea8ab8437ac0862e0151
oleg-gonchar <oleg.gonchar@valtech.com>
2020-06-17 15:05:48 +0300

    event list container and event list item

---
+++ src/app/components/event-list-item/event-list-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ConditionalExpr } from '@angular/compiler';
+
+@Component({
+    selector: 'app-event-list-item',
+    templateUrl: 'event-list-item.component.html',
+    styleUrls: ['event-list-item.component.scss']
+})
+
+export class EventListItem implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+}
---
+++ src/app/components/event-list/event-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ConditionalExpr } from '@angular/compiler';
+
+@Component({
+    selector: 'app-event-list',
+    templateUrl: 'event-list.component.html',
+    styleUrls: ['event-list.component.scss']
+})
+
+export class EventList implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+}

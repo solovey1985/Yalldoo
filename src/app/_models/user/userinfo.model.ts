3c54b2561aca62edbd4d4eda99e4902ce7eaf1fb
Andrii Solovienko <osobuy@gmail.com>
2020-07-22 00:03:42 +0300

    [spa][profile edit] birthday

---
+++ src/app/_models/user/userinfo.model.ts
@@ -0,0 +1,13 @@
+import { PreferencesComponent } from "app/pages/preferences/preferences.component";
+
+export default class UserInformation {
+    
+    firstName: string;
+    lastName: string;
+    birthDate: Date ;
+    description: string;
+    
+    constructor() {
+        
+    }
+}
--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -5,6 +5,9 @@ import { CategoryService } from "app/_services/category/category.service";
 import LocationDto from "app/_models/location.dto";
 import { Category } from "app/_models/category/category.model";
 import { ThrowStmt } from "@angular/compiler";
+import { User } from "app/_models";
+import UserInformation from "app/_models/user/userinfo.model";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
     selector: "app-settings",
@@ -18,17 +21,31 @@ export class SettingsComponent implements OnInit {
     locations: LocationDto[];
     data: Date = new Date();
     categories: Category[];
-    constructor(private modal: ModalService, private notify: NotifyService, private categoryService: CategoryService) {
+    user: User;
+    userInfo: UserInformation;
+    form: FormGroup;
+    validation_messages: any;
+    constructor(private modal: ModalService,
+        private notify: NotifyService,
+        private categoryService: CategoryService,
+        private fb: FormBuilder) {
         this.locations = new Array<LocationDto>();
+        this.userInfo = new UserInformation();
+        this.userInfo.birthDate = new Date('05.15.1985');
+        this.initValidationMessages();
     }
 
     ngOnInit() {
+
+        //TODO: Remove 
         var body = document.getElementsByTagName("body")[0];
         body.classList.add("settings-page");
 
         this.categories = this.categoryService.getChildCategories().filter(x => x.id.toLocaleString().endsWith('02'));
+        this.buildForm();
     }
     ngOnDestroy() {
+        //TODO: Remove 
         var body = document.getElementsByTagName("body")[0];
         body.classList.remove("settings-page");
     }
@@ -57,5 +74,31 @@ export class SettingsComponent implements OnInit {
 
     getIcon(title: string): string{
         return this.categoryService.getCategoryIcon(title);
+    }
+
+   private initValidationMessages() {
+        this.validation_messages = {
+            privacy: [{ type: "required", message: "Select event privacy" }],
+            firstName: [
+                { type: "required", message: "Title is required" },
+                { type: "minlength", message: "Title must be at least 3 characters long" },
+                { type: "maxlength", message: "Title cannot be more than 50 characters long" }
+            ],
+            birthDate: [{ type: "required", message: "Date is required" }],
+            location: [{ type: "required", message: "Location is required" }],
+            category: [{ type: "required", message: "Category is required" }],
+            description: [{ type: "maxlength", message: "Description cannot be more than 1024 characters long" }]
+        };
+    }
+
+    private buildForm(): void{
+        this.form = this.fb.group({
+            firstName: [this.userInfo.firstName, [Validators.required]],
+            lastName: [this.userInfo.lastName],
+            birthDate: [this.userInfo.birthDate],
+            description: [this.userInfo.description],
+            categories: [''],
+            places: [''],
+        }, {updateOn:"change"});
     }
 }

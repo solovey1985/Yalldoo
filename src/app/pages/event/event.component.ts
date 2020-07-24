6b06cb2ebd42a55950051f0c8780076139adc33e
Andrii Solovienko <osobuy@gmail.com>
2020-07-24 20:19:38 +0300

    [spa][event flow] init page

--- src/app/pages/event/event.component.ts
+++ src/app/pages/event/event.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+  styleUrls: ['./event.component.scss']
 })
 export class EventComponent implements OnInit {
 
+  date = new Date(2020, 8, 24, 12, 20, 0);
   constructor() { }
 
   ngOnInit(): void {

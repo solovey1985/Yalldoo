19f5715d282f33cc2cdc0ebfb4f43e512edf52c0
oleg-gonchar <oleg.gonchar@valtech.com>
2020-11-29 01:41:02 +0200

    Profile event list item starting. All categories pictures and icons

---
+++ src/app/components/profile-event-list-item-my/event-list-item-my.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { ConditionalExpr } from "@angular/compiler";
+import { EventModel } from "app/_models/events/event.model";
+
+@Component({
+    selector: "app-event-list-item-my",
+    templateUrl: "event-list-item-my.component.html",
+    styleUrls: ["event-list-item-my.component.scss"]
+})
+
+export class EventListItemMy implements OnInit {
+    @Input()
+    event = {
+        title: 'Super event',
+        description: 'This is the best event in the world ever',
+        location: {
+            address: 'Shevchenko street 13'
+        },
+        startDate: new Date()
+    };
+
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+}
